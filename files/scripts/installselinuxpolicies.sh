d7f2859e886763872c1d133391f2f9cd6af96d0a
webmaster <webmaster@mail.local>
2025-10-25 22:46:06 +0200

    happydays

--- files/scripts/installselinuxpolicies.sh
+++ files/scripts/installselinuxpolicies.sh
@@ -5,6 +5,6 @@ rpm-ostree install selinux-policy-devel
 
 cd ./selinux/google_chrome/
 make -f /usr/share/selinux/devel/Makefile googlechrome.pp
-semodule -v -i ./selinux/google_chrome/googlechrome.pp
 
+semodule -v -i googlechrome.pp
 restorecon -FRv /lib/opt/google/chrome/
