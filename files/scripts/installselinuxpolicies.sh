3f7b2709480cdfc1184842885a65b0cf64795409
webmaster <webmaster@mail.local>
2025-10-26 00:17:57 +0200

    a

--- files/scripts/installselinuxpolicies.sh
+++ files/scripts/installselinuxpolicies.sh
@@ -3,12 +3,15 @@
 set -oue pipefail
 rpm-ostree install selinux-policy-devel
 
+setenforce 0
+
 cd ./selinux/google_chrome/
 make -f /usr/share/selinux/devel/Makefile googlechrome.pp
 
 semodule -v -i googlechrome.pp
 echo 'Restoring permissions:'
 restorecon -FRv /opt/
+restorecon -FRv /usr/
 
-
+setenforce 1
 #chcon -t googlechrome_exec_t /usr/lib/opt/google/chrome/chrome
