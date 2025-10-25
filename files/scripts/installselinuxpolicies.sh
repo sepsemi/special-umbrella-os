8112d900bf104851436f3e7c792cf73b431332a3
webmaster <webmaster@mail.local>
2025-10-26 00:12:21 +0200

    revolutionary?

--- files/scripts/installselinuxpolicies.sh
+++ files/scripts/installselinuxpolicies.sh
@@ -8,7 +8,7 @@ make -f /usr/share/selinux/devel/Makefile googlechrome.pp
 
 semodule -v -i googlechrome.pp
 echo 'Restoring permissions:'
-restorecon -FRv /usr/
+restorecon -FRv /opt/
 
 
 #chcon -t googlechrome_exec_t /usr/lib/opt/google/chrome/chrome
